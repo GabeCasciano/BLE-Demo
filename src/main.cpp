c758be7726908243cd45e8906f7935b6c3cafdbb
GabeCasciano <gabecasciano@gmail.com>
2025-10-31 20:18:10 -0400

    adadfruit libs from gpt don't work, went old school and found this

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#include <Adafruit_Microbit.h>
+#include <Adafruit_MAG3110.h>  // magnometer library
+#include <Adafruit_MMA8653.h>  // accelerometer library
+#include <Adafruit_Microbit.h> // base microbit library
+#include <Adafruit_Sensor.h>   // adafruit sensor helper
 
-#include "Logger.h"
+#include "Logger.h" // custome logger
 
 Adafruit_Microbit uBit;
+Adafruit_MMA8653 accel;
+Adafruit_MAG3110 mag;
 
 void setup() {
   // initialize the logger & serial port
@@ -16,7 +21,12 @@ void setup() {
   Wire.begin(); // initialize I2C for sensors
   uBit.begin(); // initialize micrbot object
 
-  uBit.matrix.print('G');
+  if (!accel.begin()) {
+    LOGGER(ERROR, "MMA8653 not found on I2C (check wiring/version)");
+  } else {
+    accel.setRange(MMA8653_RANGE_2_G); // 2/4/8g
+    LOGGER(INFO, "MMA8653 ready");
+  }
 }
 
 void loop() {}
