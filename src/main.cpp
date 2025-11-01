7d0ec3ac3a5686fa3ed2e3bd3cbc5be1db1213f3
GabeCasciano <gabecasciano@gmail.com>
2025-10-31 20:21:55 -0400

    everything is compiling now which is nice

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#include <Adafruit_MAG3110.h>  // magnometer library
-#include <Adafruit_MMA8653.h>  // accelerometer library
 #include <Adafruit_Microbit.h> // base microbit library
-#include <Adafruit_Sensor.h>   // adafruit sensor helper
+#include <SparkFun_MAG3110.h>
+
+#include "MMA8653.h"
 
 #include "Logger.h" // custome logger
 
 Adafruit_Microbit uBit;
-Adafruit_MMA8653 accel;
-Adafruit_MAG3110 mag;
 
 void setup() {
   // initialize the logger & serial port
@@ -20,13 +18,6 @@ void setup() {
 
   Wire.begin(); // initialize I2C for sensors
   uBit.begin(); // initialize micrbot object
-
-  if (!accel.begin()) {
-    LOGGER(ERROR, "MMA8653 not found on I2C (check wiring/version)");
-  } else {
-    accel.setRange(MMA8653_RANGE_2_G); // 2/4/8g
-    LOGGER(INFO, "MMA8653 ready");
-  }
 }
 
 void loop() {}
